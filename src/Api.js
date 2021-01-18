26279735e2b31928801bf0092943c068b1b31d23
Eniola Ajiboye <eloyjaws@gmail.com>
2021-01-18 05:41:35 +0200

    Update API Url to use https

--- src/Api.js
+++ src/Api.js
@@ -1,5 +1,5 @@
 const API_KEY = process.env.REACT_APP_OMDB_SECRET;
-const API_URL = `http://www.omdbapi.com/?apikey=${API_KEY}&s=`;
+const API_URL = `https://www.omdbapi.com/?apikey=${API_KEY}&s=`;
 
 export function fetchMovieList(movieTitle) {
   return fetch(API_URL + encodeURI(movieTitle)).then(res => res.json());
